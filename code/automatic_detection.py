6e5bf3f39a944a51b7e6f249e1ca19ef76b4dd94
1603957 <101736344+1603957@users.noreply.github.com>
2023-05-22 15:31:00 +0200

    Corregit l'error en assumpció a l'hora de separar els segments

--- code/automatic_detection.py
+++ code/automatic_detection.py
@@ -6,6 +6,7 @@ def automatic_detection(image):
     image_blur = cv2.GaussianBlur(image, (5, 5), 0)
     rows,cols = image_blur.shape
     image_cut = image_blur[400:int(rows*3/4),:]
+    cv2.imshow('1',image_cut)
     sum_l = np.zeros(cols)
     for i in range(cols):
         if sum(image_cut[:,i]) > 0:
@@ -25,22 +26,21 @@ def automatic_detection(image):
     c = False
     for pix_n in range(len(sum_l)):
         if sum_l[pix_n] != sum_l[pix_ant]:
-            if not c:
+            if sum_l[pix_n] == 255.0:
                 inicio_b = pix_n
                 final_n = pix_ant
-                c = True
             else:
                 final_b = pix_ant
                 long_coche_if.append([inicio_b,final_b])
                 separaciones_if.append([inicio_n,final_n])
                 inicio_n = pix_n
-                c = False
         pix_ant = pix_n
     separaciones_if.append([inicio_n,cols])
     separaciones_if = separaciones_if[1:]
     
     for ifin in separaciones_if:
         separaciones.append(abs(ifin[1]-ifin[0]))
+        cv2.imshow('2',image_cut[:,ifin[0]:ifin[1]])
     for ifin in long_coche_if:
         long_coche.append(abs(ifin[1]-ifin[0]))
 
@@ -62,5 +62,5 @@ def automatic_detection(image):
                 estado_plazas.append(0)
         else:
             estado_plazas.append(1)
-            
+    print(len(estado_plazas))        
     return estado_plazas
